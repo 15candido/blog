96144b7c60fd4964282a076247a792412748e2e7
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-11-16 00:14:11 +0000

    V016_11_23_00h05. Route for category's menu dropdown

--- routes/web.php
+++ routes/web.php
@@ -18,25 +18,30 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('posts', [
-        "posts" => Post::with('category', 'author')->get()
+        "posts" => Post::with('category', 'author')->get(),
+        "categories" => Category::all()
     ]);
 });
 
 Route::get('posts/{post:slug}', function (Post $post) {
     return view("post", [
         "post" => $post
+       
     ]);
 });
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        "posts" => $category->posts->load(['category', 'author'])
+        'posts' => $category->posts->load(['category', 'author']),
+        'currentCategory' => $category,
+        'categories' => Category::all()
     ]);
 });
 
 Route::get('authors/{author:username}', function (User $author) {
     // dd($author);
     return view('posts', [
-        "posts" => $author->posts->load(['category', 'author'])
+        "posts" => $author->posts->load(['category', 'author']),
+        "categories" => Category::all()
     ]);
 });
