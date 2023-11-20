df08a08e58cebb25fbd9b1f1b1ae612961b8339a
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-11-20 23:32:23 +0000

    V020_11_23_23h31. Arranging layout

--- resources/views/components/posts/post-featured-card.blade.php
+++ resources/views/components/posts/post-featured-card.blade.php
@@ -10,7 +10,7 @@
         <div class="flex-1 flex flex-col justify-between">
             <header class="mt-8 lg:mt-0">
                 <div class="space-x-2">
-                    <a href="/categories/{{$post->category->slug}}" class="px-3 py-1 border border-blue-300 rounded-full
+                    <a href="/categories/{{$post->category->slug}}" class="px-3 py-1 space-y-4 border border-blue-300 rounded-full
                      text-blue-300 text-xs uppercase font-semibold" style="font-size: 10px">
                         {{$post->category->name}}
                     </a>
@@ -34,10 +34,10 @@
                 </div>
             </header>
 
-            <div class="text-sm mt-2">
-                <p>
-                    {{$post->excerpt}}
-                </p>
+            <div class="text-sm mt-2 space-y-4">
+               
+                {!! $post->excerpt !!}
+              
 
                 <!-- <p class="mt-4">
                     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
@@ -54,7 +54,8 @@
                 </div>
 
                 <div class="hidden lg:block">
-                    <a href="/posts/{{$post->slug}}" class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8">Read More</a>
+                    <a href="/posts/{{$post->slug}}" class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 
+                    rounded-full py-2 px-8">Read More</a>
                 </div>
             </footer>
         </div>
