4efe24c4e6fc3d000005f5b77e47dab8c75c8954
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-11-25 00:08:21 +0000

    V025_11_23_00h09. Marege category and search query

--- resources/views/posts/_carousel.blade.php
+++ resources/views/posts/_carousel.blade.php
@@ -31,8 +31,12 @@
 
         <!-- Search -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
-            <form method="GET" action="#">
-                <input type="text" name="search" placeholder="Search" value="{{request('search')}}" class="bg-transparent placeholder-black font-semibold text-sm">
+            <form method="GET" action="/">
+                @if(request('category'))
+                    <input type="hidden" name="category" value="{{ request('category') }}">
+                @endif
+                <input type="text" name="search" placeholder="Search" value="{{request('search')}}" 
+                class="bg-transparent placeholder-black font-semibold text-sm">
             </form>
         </div>
     </div>
