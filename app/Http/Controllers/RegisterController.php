a7c70bd53a3597145cb94c09381e46f24978b244
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-11-30 12:08:21 +0000

    V030_11_23_12h8. Setting unique to username and email attributes.

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -17,13 +17,13 @@ class RegisterController extends Controller
 
     //    return request()->all(); 
        
-       User::create(request()->validate([
+     User::create(request()->validate([
 
             'name'      => ['required', 'min:7', 'max:255'],
-            'username'  => ['required', 'min:7', 'max:255'],
-            'email'    =>  ['required','email:rfc', 'min:7', 'max:255'],
-            'password'  => ['required', 'min:5', 'max:255']
-            
+            'username'  => ['required', 'min:7', 'max:255', 'unique:users,username'],
+            'email'    =>  ['required','email:rfc', 'min:7', 'max:255', 'unique:users,email'],
+            'password'  => ['required', 'min:9', 'max:255']
+
         ]));
 
         return redirect('/'); 
