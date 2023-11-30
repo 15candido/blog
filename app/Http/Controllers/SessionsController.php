847a456b91d9e1c0bc114336d653d0497b2a6e57
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-11-30 20:01:52 +0000

    V030_11_23_20h2. Login user

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,13 +2,53 @@
 
 namespace App\Http\Controllers;
 
-
+use Dotenv\Exception\ValidationException;
+use Illuminate\Validation\ValidationException as ValidationValidationException;
 
 class SessionsController extends Controller
 {
-    public function destroy() {
-        auth()->logout();
 
+    public function create()
+    {
+
+        return view('login.create');
+    }
+
+    public function store()
+    {
+
+        // Validate the request
+
+        $attributes = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        // Attempt to authenticate and login the user based on the provided credentials 
+        if (auth()->attempt($attributes)) {
+
+            // Session fixation to prevent 
+            session()->regenerate();
+
+            // Redirect with a success Flash message 
+            return redirect('/')->with('success', 'Hello welcome back! ');
+        }
+
+        // Authentication failed.
+
+        throw ValidationValidationException::withMessages([
+            'email' => 'Sorry, your provided credentials could not be verifeid!'
+        ]);
+
+        // return back()
+        //     ->withInput()
+        //     ->withErrors(['email' => 'Sorry, your porvided credentials could not be verified']);
+    }
+
+    public function destroy()
+    {
+
+        auth()->logout();
         return redirect('/')->with('success', 'Goodba!');
     }
 }
