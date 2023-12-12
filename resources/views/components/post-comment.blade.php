774f6a50d89dafe5ca79db9ebe053914eadcde59
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-12-12 17:20:53 +0000

    V012_12_23_17h10. New component for the comment view.

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,19 +1,20 @@
+@props(['comment'])
 <div class="max-w mx-auto bg-gray-100 border border-gray-200 shadow-md rounded-xl overflow-hidden md:max-w-full">
     <div class="md:flex ">
         {{-- User avatar --}}
         <div class="md:flex-shrink-0">
-            <img src="https://i.pravatar.cc/192" alt="user-profil" class="w-full h-36 object-cover md:w-36 md:h-full">
+            <img src="https://i.pravatar.cc/192?u={{$comment->id}}" alt="user-profil" class="w-full h-36 object-cover md:w-36 md:h-full">
         </div>
 
         {{-- Comment Detalais --}}
         <div class="w-full p-6">
             <header class="space-y-1">
-                <h3 class="font-semibold text-sm text-blue-400 tracking-wide uppercase">Joe Doe</h3>
-                <p class="text-xs">Posted<time>8 month ago!</time></p>
+                <h3 class="font-semibold text-sm text-blue-400 tracking-wide uppercase">{{$comment->author->name}}</h3>
+                <p class="text-xs">Posted <time> {{$comment->created_at->diffForHumans()}}</time></p>
             </header>
             <div class="mt-2">
                 <p class="text-gray-500">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    {{$comment->body}}
                 </p>
             </div>
         </div>
